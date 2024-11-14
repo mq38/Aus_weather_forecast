97d63d2fcb4efedd8acd0be41db92b0f2f83a73b
mq38 <mihkelpaal123@gmail.com>
2024-11-14 13:40:15 +0200

    andmed loetud

--- code.py
+++ code.py
@@ -1,6 +1,10 @@
-import math as m
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # data visualization
+import seaborn as sns # statistical data visualization
 
-# model.fit()
-# profit.claim()
+data = "C:\projekt\Andmed\weatherAUS.csv"
+df = pd.read_csv(data)
 
-print("MIHKEL")
+
+print(df.head())
