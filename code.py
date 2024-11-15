87942e137294f73b3cd5bf802c21bb2101304931
mq38 <mihkelpaal123@gmail.com>
2024-11-15 11:06:45 +0200

    nyyd jookseb yks kord kood

--- code.py
+++ code.py
@@ -1,10 +1,14 @@
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
 import matplotlib.pyplot as plt # data visualization
-import seaborn as sns # statistical data visualization
+import seaborn as sns # data visualization
+sns.reset_defaults()
+
 
 data = "C:\projekt\Andmed\weatherAUS.csv"
 df = pd.read_csv(data)
+df.info()
 
-
-print(df.head())
